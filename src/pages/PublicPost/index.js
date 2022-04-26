0c276e06c9aaa4b1c3e265b3712e95eaf9638b51
DavidZYao <davidyao.aus@gmail.com>
2022-04-26 12:03:11 +1000

    stable version

--- src/pages/PublicPost/index.js
+++ src/pages/PublicPost/index.js
@@ -1,12 +1,14 @@
-
-import Navbar from "../../common/layout/Navbar";
-import Article from "../../common/layout/Article";
-import Filter from "../../common/layout/Filter";
-import { PublicPostPage, PostWrapper } from "./PublicPostElement";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { loadPosts, selectFirstNPosts } from "../../store/posts";
-import { NavButton } from "./PublicPostElement";
+import Article from '../../common/layout/Article';
+import Filter from '../../common/layout/Filter';
+import {
+  PublicPostPage,
+  PostContainer,
+  PostWrapper,
+} from './PublicPostElement';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { loadPosts, selectFirstNPosts } from '../../store/posts';
+import { NavButton } from './PublicPostElement';
 
 const PublicPost = () => {
   const dispatch = useDispatch();
@@ -18,14 +20,14 @@ const PublicPost = () => {
   return (
     <>
       <PublicPostPage>
-        <Navbar />
         <Filter />
-
-        <NavButton to='/create_post'>Post</NavButton>
+        <NavButton to="/create_post">Post</NavButton>
         <PostWrapper>
-          {posts.map((post) => (
-            <Article key={post._id} post={post} />
-          ))}
+          <PostContainer>
+            {posts.map((post) => (
+              <Article key={post._id} post={post} />
+            ))}
+          </PostContainer>
         </PostWrapper>
       </PublicPostPage>
     </>
